ac44c383d791c2bbb0d22bbdbc6efcd40ec5b17d
vinay nair <vinaynair21@gmail.com>
2021-10-04 06:22:11 +0530

    shell

---
+++ update.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+
+#
+# Make sure installed packages are up to date with all security patches.
+#
+sudo apt-get -yq update
+
+
+#
+# Install python3
+#
+sudo apt-get install python3
+
+#
+# Install python pip
+#
+sudo apt-get install python-pip3
+
+#
+# Install git 
+#
+sudo apt-get install git
